218d270074280a9d6a27b31e2b80bc8fec6796ef
Fu, Tommy <tommy.fu@sap.com>
2017-07-23 18:50:08 +0800

    router.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const http = require('http');
-const routes = require('./src/routers');
+const routers = require('./src/routers');
 
 let app = express();
-routes(app);
+routers(app);
 http.createServer(app).listen(3000, function() {
   console.log('Server listening on port 3000');
 });
